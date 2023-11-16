8958add8f513cdfd50bbdcb9355be64457ab7724
HeyprincesHere <125874506+HeyprincesHere@users.noreply.github.com>
2023-11-16 13:01:27 +0530

    Add files via upload

---
+++ if statement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 10; // Define and initialize 'x'
+        int y = 20; // Define and initialize 'y'
+
+        if (x > y)
+        {
+            Console.WriteLine("x is greater");
+        }
+        else
+        {
+            Console.WriteLine("x is not greater");
+        }
+    }
+}
