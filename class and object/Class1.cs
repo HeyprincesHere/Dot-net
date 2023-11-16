796884b91107c02e527d58a13b2f5d12fe3a2239
HeyprincesHere <125874506+HeyprincesHere@users.noreply.github.com>
2023-11-16 12:58:00 +0530

    Add files via upload

---
+++ class and object/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_and_object
+{
+    internal class scooty
+        
+        {
+            string brand = "vespa";
+            int speed = 100;
+        }
+}
---
+++ class and object/Program.cs
@@ -0,0 +1,53 @@
+﻿//using System; 
+
+//class car 
+//{
+//    string color = " red";
+//    static void Main()
+//    {
+//        car myobj = new car();  
+//        Console.WriteLine("my car color is:"+myobj.color.ToString());
+//    }
+//}
+
+// Multiple objects in class:
+
+//using System;
+//using System.Runtime.ConstrainedExecution;
+
+//class Car
+//{
+//    string color = "red";
+//    string brand = "Rolce Royce";
+//    static void Main()
+//    {
+//        Car myObj1 = new Car();
+//        Car myObj2 = new Car();
+//        string carInfo = ($"My car color is {myObj1.color} and the brand is {myObj2.brand}");
+//        Console.WriteLine(carInfo);
+
+//    }
+
+//}
+
+// class member
+
+using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        scooty myobj1 = new scooty();
+        scooty myobj2 = new scooty();
+        
+    }
+    
+
+       
+}
+  
+
+
+
