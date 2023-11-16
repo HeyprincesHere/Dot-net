a2d7ab27c6530cf23a4d90ec7dffa02bec2569d6
HeyprincesHere <125874506+HeyprincesHere@users.noreply.github.com>
2023-11-16 13:02:50 +0530

    Add files via upload

---
+++ custom exception/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class MyCustomException : Exception
+{
+    public MyCustomException()
+    {
+    }
+    public MyCustomException(string message)
+        : base(message)
+    {
+
+    }
+    public MyCustomException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+
+    }
+}
+
+public class CustomExceptionDemo
+{
+    public void DoSomething()
+    {
+        // Simulate an error
+        throw new MyCustomException("Something went wrong in DoSomething()");
+    }
+
+    public static void Main()
+    {
+        try
+        {
+            CustomExceptionDemo demo = new CustomExceptionDemo();
+            demo.DoSomething();
+        }
+        catch (MyCustomException ex)
+        {
+            Console.WriteLine("Custom Exception caught: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            // Handle other exceptions
+            Console.WriteLine("Exception caught: " + ex.Message);
+        }
+    }
+}
+
+
