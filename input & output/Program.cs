ef6ee084e0f0f5385714644872eeedaf94d154ce
HeyprincesHere <125874506+HeyprincesHere@users.noreply.github.com>
2023-11-16 13:11:33 +0530

    Add files via upload

---
+++ input & output/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+ 
+        
+        Console.WriteLine($"Hello, {name}!");
+
+        
+        Console.Write("Enter an integer: ");
+        int number = int.Parse(Console.ReadLine());
+
+        
+        int doubledNumber = number * 2;
+        Console.WriteLine($"Twice the number you entered is: {doubledNumber}");
+    }
+}
