57e2ee44820d6856caa7e7ff9940afa9c263900a
HeyprincesHere <125874506+HeyprincesHere@users.noreply.github.com>
2023-11-16 14:10:09 +0530

    Add files via upload

---
+++ parameter & argument/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Car
+{
+    static void car(string brand1, string brand2, string brand3 = "honda")
+    {
+        Console.WriteLine("my car brand is :" + brand1);
+        Console.WriteLine("my car brand is :" + brand2);
+        Console.WriteLine("my car brand is :" + brand3);
+
+    }
+    static void Main()
+    {
+        car(brand1: "volovo", brand2: "RR");
+
+    }
+}
