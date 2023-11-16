3f8eaddc7fbf7846148251597f27066217b76595
HeyprincesHere <125874506+HeyprincesHere@users.noreply.github.com>
2023-11-16 14:11:25 +0530

    Add files via upload

---
+++ reverse num/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class ReverseExample
+{
+    static void Main()
+    {
+        int n, reverse = 0, rem;
+        Console.WriteLine("eneter a number");
+        n = int .Parse(Console.ReadLine());
+        while (n!=0)
+        {
+            rem = n % 10;
+            reverse = reverse * 10 + rem;
+            n /= 10;
+
+        }
+        Console.WriteLine("reverse number is :" + reverse);
+
+
+    }
+}
+
