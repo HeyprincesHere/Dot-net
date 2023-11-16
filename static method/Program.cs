e91eda7e194dd929ffe48b003e08a7ecd37b2847
HeyprincesHere <125874506+HeyprincesHere@users.noreply.github.com>
2023-11-16 14:12:27 +0530

    Add files via upload

---
+++ static method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    
+    static int mult(int a, int b)
+    {
+        return a * b;
+    }
+
+    static void Main()
+    {
+        
+        int result = mult(100, 200);
+
+        
+        Console.WriteLine($"Result: {result}");
+    }
+}
